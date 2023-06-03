73eed0ca6d404ce540d4c64ade6c63e6c2e9a201
Nicolas Arnouts <nicolas.arnouts@outlook.com>
2023-06-03 22:18:49 +0200

    Added tailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: false, // or 'media' or 'class'
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
