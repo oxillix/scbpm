78726503ff2c62c5f62ca34e1d5158384b65f7f2
Nicolas Arnouts <nicolas.arnouts@outlook.com>
2023-06-03 23:48:18 +0200

    Trying to get trackItems in a table

--- src/sbpm.ts
+++ src/sbpm.ts
@@ -1,5 +1,6 @@
 import { io, Socket } from "socket.io-client";
 import { socketManager } from "./services/sockets/main";
+import { debounce } from "lodash"; // for mutationObserver
 
 console.log("OKaaay lets go sbpm.ts yigha");
 const BATCH_SIZE = 10; // adjust this to the desired batch size
@@ -23,10 +24,15 @@ const getCompactTracklistItems = () => {
       const trackTitleElement = item.querySelector(
         ".compactTrackListItem__trackTitle"
       );
-      const trackTitle = trackTitleElement.innerHTML.replace("-", "").trim().replace('&amp;', '&');
+      const trackTitle = trackTitleElement.innerHTML
+        .replace("-", "")
+        .trim()
+        .replace("&amp;", "&");
       console.log("Track Title: ", trackTitle);
 
-      const trackUrlPath = trackTitleElement.getAttribute("data-permalink-path");
+      const trackUrlPath = trackTitleElement.getAttribute(
+        "data-permalink-path"
+      );
       console.log("Permalink Path: ", trackUrlPath);
     });
   });
@@ -43,6 +49,9 @@ const getPlaylistTracklistItems = () => {
 
   console.log("Playlist track items:", trackItems);
 
+  // Create an array to store the table rows
+  const tableRows: any = [];
+
   trackItems.forEach((item) => {
     const trackTitleElement = item.querySelector(".trackItem__trackTitle");
 
@@ -52,12 +61,44 @@ const getPlaylistTracklistItems = () => {
     const trackTitle = trackTitleElement.textContent.trim();
     console.log("Track Title: ", trackTitle);
 
-    const userElement = item.querySelector(".trackItem__username");
-    const trackUser = trackTitleElement.textContent.trim();
-    console.log("Track User: ", trackUser);
-
     const playCountElement = item.querySelector(".trackItem__playCount");
     console.log("Playlist Track Play Count: ", playCountElement.textContent);
+
+    // Generate a random BPM value for the row
+    const bpm = Math.floor(Math.random() * 100) + 60;
+
+    // Generate a random key value for the row
+    const keys = [
+      "C",
+      "C#",
+      "D",
+      "D#",
+      "E",
+      "F",
+      "F#",
+      "G",
+      "G#",
+      "A",
+      "A#",
+      "B",
+    ];
+    const key = keys[Math.floor(Math.random() * keys.length)];
+
+    // Create a table row HTML string with the track information and mock BPM/Key values
+    const row = `
+    <tr class="flex">
+      <td>
+        <div class="">
+          ${item.innerHTML}
+        </div>
+      </td>
+      <td>${bpm}</td>
+      <td>${key}</td>
+    </tr>
+  `;
+
+    // Push the row HTML string to the tableRows array
+    tableRows.push(row);
   });
 
   if (trackItems.length === 0) {
@@ -65,12 +106,52 @@ const getPlaylistTracklistItems = () => {
   } else {
     console.log("Trackitems is not empty: ", trackItems.length);
   }
+
+  stopDOMObserver();
+
+  // Construct the table HTML using the tableRows array and insert it into the trackList container
+  trackList.innerHTML = `
+  <div class="w-full bg-red-400">
+    <table>
+      <thead>
+        <tr>
+          <th>Track</th>
+          <th>BPM</th>
+          <th>Key</th>
+        </tr>
+      </thead>
+      <tbody>
+        ${tableRows.join("")}
+      </tbody>
+    </table>
+    </div>
+  `;
+
+  // trackItems[0].innerHTML
+
+  // trackList.innerHTML = `
+  // <div class="">
+  // ${trackItems[0].innerHTML}
+  // </div>`
+  //trackList.innerHTML = '<div class="bg-red-200 text-blue-500">aaaallooo</div>'
+
+  startDOMObserver();
 };
 
-const observer = new MutationObserver(async () => {
+const observerCallback = debounce(async () => {
   getCompactTracklistItems();
   getPlaylistTracklistItems();
-});
+}, 1000); // Debounce for 1 second (adjust the delay as needed)
 
-// Start observing the document with the configured parameters
-observer.observe(document, { childList: true, subtree: true });
+const startDOMObserver = () => {
+  // Reconnect the observer after modifications are done
+  observer.observe(document, { childList: true, subtree: true });
+};
+
+const stopDOMObserver = () => {
+  // Disconnect the observer before making modifications
+  observer.disconnect();
+};
+
+const observer = new MutationObserver(observerCallback);
+startDOMObserver();
